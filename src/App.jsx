214309520a69079b65aa6d990a09142e9456146c
zurajanai-katsurada <antusharma6624@gmail.com>
2023-09-23 13:17:13 +0600

    library especially for learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl bg-red-300">Knowledge Cafe</h1>
+      <Header></Header>
     </>
   );
 }
